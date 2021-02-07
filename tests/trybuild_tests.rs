c7e34c1e6891417b837c7ff107142b31c19a403a
mobile-bungalow <pwmay@ucsc.edu>
2021-02-07 15:48:28 -0800

    added testing harness and basic negative behavior tests

---
+++ tests/trybuild_tests.rs
@@ -0,0 +1,12 @@
+#[test]
+#[cfg(feature = "derive")]
+fn derive() {
+    let tester = trybuild::TestCases::new();
+    tester.pass("tests/trybuild_tests_src/derive.rs");
+    tester.compile_fail("tests/trybuild_tests_src/pod_derive_failure.rs");
+    tester.compile_fail("tests/trybuild_tests_src/transparentwrapper_failure.rs");
+    tester.compile_fail("tests/trybuild_tests_src/nonzeroable_fields.rs");
+    tester.compile_fail("tests/trybuild_tests_src/contiguous_derive_failure.rs");
+}
+
+
