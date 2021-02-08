8ba8aaa1107150a3756d5a1363112133dfe16b81
mobile-bungalow <pwmay@ucsc.edu>
2021-02-08 10:42:33 -0800

    derive tests now only run on stable.

--- tests/trybuild_tests_src/derive.rs
+++ tests/trybuild_tests_src/derive.rs
@@ -1,25 +1,32 @@
-#![cfg(feature = "derive")]
-#![allow(dead_code)]
+use bytemuck::{Zeroable, Pod, TransparentWrapper, Contiguous};
 
-use bytemuck::{Zeroable, Pod, TransparentWrapper};
+#[derive(Contiguous, Copy, Clone)]
+#[repr(u8)]
+enum ContEnum {
+    A = 11,
+    B = 12,
+    C = 13,
+}
 
 #[derive(Copy, Clone, Pod, Zeroable)]
 #[repr(C)]
 struct Test {
-  a: u16,
-  b: u16,
+    a: u16,
+    b: u16,
 }
 
 #[derive(TransparentWrapper)]
 #[repr(transparent)]
 struct TransparentSingle {
-  a: u16,
+    a: u16,
 }
 
 #[derive(TransparentWrapper)]
 #[repr(transparent)]
 #[transparent(u16)]
 struct TransparentWithZeroSized {
-  a: u16,
-  b: ()
+    a: u16,
+    b: ()
 }
+
+fn main() {}
